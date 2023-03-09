07a86307c149586a94897195d384d2c442bf00f8
Sergey Mazur <srgmazur@gmail.com>
2023-03-09 09:54:20 +0300

    Update query cheat-list.sql

--- query cheat-list.sql
+++ query cheat-list.sql
@@ -36,3 +36,14 @@ from
 order by
   birth_year desc,
   courier_id
+  
+  ---
+  
+  select
+  courier_id,
+  coalesce (date_part ('year', birth_date) :: varchar, 'unknown') as birth_year
+from
+  couriers
+order by
+  birth_year desc,
+  courier_id
