a0365855900de4f41a7d06d706602f5ca0044ab4
Sergey Mazur <srgmazur@gmail.com>
2023-03-09 09:40:14 +0300

    Update query cheat-list.sql

--- query cheat-list.sql
+++ query cheat-list.sql
@@ -25,3 +25,14 @@ select
   price :: varchar
 from
   products
+
+-----
+
+select
+  courier_id,
+  date_part ('year', birth_date) as birth_year
+from
+  couriers
+order by
+  birth_year desc,
+  courier_id
