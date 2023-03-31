b14613a797d357461ef5e980b078a5f931325e2d
Sergey Mazur <srgmazur@gmail.com>
2023-03-31 19:45:15 +0300

    Update query cheat-list.sql

--- query cheat-list.sql
+++ query cheat-list.sql
@@ -212,3 +212,19 @@ where
   and time between '2022-08-25' and '2022-09-05'
 order by
   order_id desc
+
+-- Определите id и даты рождения 50 самых молодых пользователей мужского пола из таблицы users.
+-- Не учитывайте тех пользователей, у которых не указана дата рождения.
+-- Поле в результирующей таблице: user_id, birth_date
+select
+  birth_date,
+  user_id
+from
+  users
+where
+  birth_date is not null and sex = 'male'
+order by
+  birth_date desc
+limit
+  50
+  
